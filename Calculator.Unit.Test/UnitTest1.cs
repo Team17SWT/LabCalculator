5472c090084f42928bc4bf66480cebf608481946
augusthjerrild <augusthjerrild@gmail.com>
2021-02-04 13:17:40 +0100

    Add project files.

---
+++ Calculator.Unit.Test/UnitTest1.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+
+namespace Calculator.Unit.Test
+{
+    public class CalculatorTest
+    {
+        private Calculator uut;
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [Test]
+        public void Add_AddTwoNumbers_Is7point5()
+        {
+            //Arrange -- Look at SetUp
+
+            //Act
+            var result = uut.Add(5, 2.5);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(7.5));
+        }
+        [Test]
+        public void Subtract_SubtractTwoNumbers_Is2()
+        {
+            //Arrange -- look at setup
+
+            //Act
+            var result = uut.Subtract(5, 3);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Multiply_MultiplyTwoNumbers_Is56()
+        {
+            //Arrange -- look at setup
+
+            //Act
+            var result = uut.Multiply(7, 8);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(56));
+        }
+        [Test]
+        public void Multiply_PowerANumber_Is25()
+        {
+            //Arrange -- look at setup
+
+            //Act
+            var result = uut.Power(5, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(25));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to the calculator!");
+            var calc = new Calculator();
+            
+            Console.WriteLine("Please enter two numbers you wish to add - if decimal, use ,");
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var add = calc.Add(a, b);
+
+            Console.WriteLine($"{a} + {b} is: {add}");
+
+            Console.WriteLine("Please enter two numbers you wish to subtract - if decimal, use ,");
+            var c = double.Parse(Console.ReadLine());
+            var d = double.Parse(Console.ReadLine());
+            var sub = calc.Subtract(c, d);
+            Console.WriteLine($"{c} - {d} is: {sub}");
+
+            Console.WriteLine("Please enter two numbers you wish to multiply - if decimal, use ,");
+            var e = double.Parse(Console.ReadLine());
+            var f = double.Parse(Console.ReadLine());
+            var mul = calc.Multiply(e, f);
+            Console.WriteLine($"{e} * {f} is: {mul} ");
+
+            Console.WriteLine("Please enter two numbers: First number is the one you want to get powered. Second is in which power - if decimal, use ,");
+            var g = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+            var pow = calc.Power(g, h);
+            Console.WriteLine($"{g} ^ {h} is {pow}");
+        }
+
+    }
+}
